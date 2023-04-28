694f5c5d6a1172d6d73847b6fbfbf7d5738788e8
Ekaterina Cherkas <ekaterina.cherkas12@mail.ru>
2023-04-28 05:17:40 +0300

    add task1 about three-digit number

---
+++ task1.py
@@ -0,0 +1,14 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+# *Пример:*
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) 
+
+i = input("Write three-digit number: ")
+sum = int(i[0]) + int(i[1]) + int(i[2])
+print(sum)
+
+# i = int(input("Write three-digit number: "))
+# a = int(i/100)
+# b = int(i/10%10)
+# c = int(i%10)
+# print(a, '+' , b , '+', c , '=' , a+b+c)
